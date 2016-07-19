5ec69df7fe16a76607430ee20aeaf07081d414dd
Jaro Reinders <jaro.reinders@gmail.com>
2016-07-19 14:47:19 +0200

    Made tests work correctly

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE RecordWildCards #-}
 module Main where
 
 --import Data.Maybe
@@ -8,6 +9,7 @@ module Main where
 --import qualified Text.Regex.TDFA.Common as TDFA (CompOption(..))
 --import qualified Text.Regex.PCRE.ByteString as PCRE
 import qualified Text.Regex.TDFA.Text as TDFAT
+import qualified Text.Regex.TDFA.Common as TDFA
 --import qualified Data.ByteString.Char8 as B
 --import Data.ByteString.Char8 (ByteString)
 --import Data.Thyme
@@ -20,12 +22,14 @@ import Control.Parallel.Strategies
 import qualified Data.Text as T
 import Data.Text (Text)
 
+import Text.Regex.TDFA.IntArrTrieSet (TrieSet (..))
+
 -- ICU
 --import qualified Data.Text.ICU as ICU
 --import qualified Data.Text.IO as T
 --import Data.Text.Foreign
---import Data.Array
 --import System.IO.Unsafe
+--import Data.Array
 
 --input :: Int -> ByteString
 --input n = B.unlines $ Prelude.replicate n "Hello world foo=123 whereas bar=456 Goodbye"
@@ -51,21 +55,30 @@ regexTDFAT = fromRight $ TDFAT.compile defaultCompOpt defaultExecOpt regexText
 --regexICU :: ICU.Regex
 --regexICU = ICU.regex [ICU.Multiline] regexText
 
+fromRight :: Either a b -> b
 fromRight (Right a) = a
 
+matchAllParallelBench :: TDFAT.Regex -> Text -> [MatchArray]
+matchAllParallelBench r t = concat (map (matchAll r) (T.lines t) `using` parList rdeepseq)
+
+main :: IO ()
 main = textBench (inputText 1) (inputText 1000) (inputText 1000000) regexTDFAT
 
 textBench :: Text -> Text -> Text -> TDFAT.Regex -> IO ()
-textBench (force -> !it1) (force -> !it1000) (force -> !it1000000) !rTDFAT = defaultMain
+textBench (force -> !it1) (force -> !it1000) (force -> !it1000000) (force -> !rTDFAT) = defaultMain
   [ bgroup "reverse"
-    [ bench "1"      $ nf T.reverse (it1)
-    , bench "1000"   $ nf T.reverse (it1000)
-    , bench "1000000" $ nf T.reverse (it1000000)
+    [ bench "1"       $ nf T.reverse it1
+    , bench "1000"    $ nf T.reverse it1000
+    , bench "1000000" $ nf T.reverse it1000000
+    ]
+  , bgroup "regex-tdfa-text parallel"
+    [ bench "1"       $ nf (uncurry matchAllParallelBench) (rTDFAT, it1) 
+    , bench "1000"    $ nf (uncurry matchAllParallelBench) (rTDFAT, it1000) 
+    --, bench "1000000" $ nf (uncurry matchAllParallelBench) (rTDFAT, it1000000) 
     ]
   , bgroup "regex-tdfa-text"
-    [ bench "1"      $ nf force ((map (matchAll rTDFAT) (T.lines $ it1) `using` parList rdeepseq))
-    , bench "1000"   $ nf force ((map (matchAll rTDFAT) (T.lines $ it1000) `using` parList rdeepseq)) 
-    , bench "1000000" $ nf force ((map (matchAll rTDFAT) (T.lines $ it1000000) `using` parList rdeepseq))
+    [ bench "1"       $ nf (uncurry matchAll) (rTDFAT, it1)
+    , bench "1000"    $ nf (uncurry matchAll) (rTDFAT, it1000)
     ]
   ]
 
@@ -86,3 +99,49 @@ textBench (force -> !it1) (force -> !it1000) (force -> !it1000000) !rTDFAT = def
 --    , bench "100"    $ nf (show . uncurry ICU.find) (regexICU, (inputText 1000))
 --    , bench "100000" $ nf (show . uncurry ICU.find) (regexICU, (inputText 100000))
 --    ]
+
+instance NFData TDFA.Regex where
+  rnf TDFA.Regex{..} = 
+    (rnf regex_dfa) `seq`
+    (rnf regex_init) `seq`
+    (rnf regex_b_index) `seq`
+    (rnf regex_b_tags) `seq`
+    (rnf regex_trie) `seq`
+    (rnf regex_tags) `seq`
+    (rnf regex_groups) `seq`
+    (rnf regex_isFrontAnchored) `seq`
+    (rnf regex_compOptions) `seq`
+    (rnf regex_execOptions) `seq`
+    ()
+
+instance NFData TDFA.DFA where 
+  rnf (TDFA.DFA a b) = rnf a `seq` rnf b `seq` ()
+
+instance NFData TDFA.DT where
+  rnf TDFA.Simple'{..} = 
+    rnf TDFA.dt_win `seq` 
+    rnf TDFA.dt_trans `seq` 
+    rnf TDFA.dt_other `seq` ()
+  rnf TDFA.Testing'{..} = 
+    rnf TDFA.dt_test `seq` 
+    rnf TDFA.dt_dopas `seq` 
+    rnf TDFA.dt_a `seq` 
+    rnf TDFA.dt_b `seq` ()
+
+instance NFData v => NFData (TrieSet v) where
+  rnf (TrieSet v n) = rnf v `seq` rnf n `seq` ()
+
+instance NFData TDFA.OP where
+  rnf TDFA.Maximize = ()
+  rnf TDFA.Minimize = ()
+  rnf TDFA.Orbit = ()
+  rnf TDFA.Ignore = ()
+
+instance NFData TDFA.GroupInfo where
+  rnf (TDFA.GroupInfo !_ !_ !_ !_ !_) = ()
+
+instance NFData TDFA.ExecOption where
+  rnf (TDFA.ExecOption !_) = ()
+
+instance NFData TDFA.CompOption where
+  rnf (TDFA.CompOption !_ !_ !_ !_ !_) = ()
